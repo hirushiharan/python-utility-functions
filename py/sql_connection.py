4f287912c39998dfb898c382779be1052278744e
Hirushiharan Thevendran <hirushiharant@gmail.com>
2024-07-18 13:26:56 +0530

    fix error

--- py/sql_connection.py
+++ py/sql_connection.py
@@ -9,10 +9,11 @@ load_dotenv()
 # MySQL database credentials
 MYSQL_ROOT_PASSWORD = os.getenv("MYSQL_ROOT_PASSWORD")
 MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
-MYSQL_USER = os.getenv("MYSQL_USER", "root") # Default to root if not set
-MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost") # Default to localhost if not set
+MYSQL_USER = os.getenv("MYSQL_USER", "root")  # Default to root if not set
+MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")  # Default to localhost if not set
 MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))  # Default to 3306 if not set
 
+connection = None
 try:
     connection = mysql.connector.connect(
         host=MYSQL_HOST,
@@ -24,10 +25,11 @@ try:
     if connection.is_connected():
         print("SQL Connection Successful")
         cursor = connection.cursor()
+        # Your database operations here
 except Error as err:
-        print(f"Error: {err}")
+    print(f"Error: {err}")
 finally:
-    if connection.is_connected():
+    if connection and connection.is_connected():
         cursor.close()
         connection.close()
         print("MySQL connection is closed")
