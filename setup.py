2ec9e12cead09b09f22e4a1a73fc35ec2ce1bb49
Hirushiharan Thevendran <hirushiharant@gmail.com>
2024-07-24 23:44:55 +0530

    fix github url typo

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 # Read the contents of your README file
 here = Path(__file__).parent
-long_description = (here / 'README.md').read_text()
 
 # Read the requirements from requirements.txt
 def parse_requirements(filename):
@@ -14,7 +13,8 @@ setup(
     name='utility-lib-python',
     version='0.1.0',
     description='A collection of utility scripts for working with files, Excel, logging, and database connections.',
-    long_description=long_description,
+    long_description="""Welcome to the Python Utils repository! This project contains a collection of utility scripts written in Python for various purposes. Each script focuses on specific functionalities that can be reused across different projects.
+    Check GitHub for more details.""",
     long_description_content_type='text/markdown',
     author='Hirushiharan',
     author_email='hirushiharant@gmail.com',
