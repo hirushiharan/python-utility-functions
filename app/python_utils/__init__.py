41b621f60cf91a44c6466aa837ccacf0bda53775
Hirushiharan Thevendran <hirushiharant@gmail.com>
2024-08-03 08:59:12 +0530

    rename structure gen file

--- app/python_utils/__init__.py
+++ app/python_utils/__init__.py
@@ -1,4 +1,4 @@
 from .src.excel_functions import ExcelReader, WriteToExcel
 from .src.file_functions import FileRenamer
 from .src.log_message import Logger
-from .src.project_structure_generator import ProjectStructure
+from .src.project_structure_gen import ProjectStructure
